960a747df53b87fed48afc4ea68e90be22bcf41b
Iveno Carolus <46677252+IvenoCarolus@users.noreply.github.com>
2020-02-02 22:15:29 +0200

    adding five_number_summary.py

---
+++ five_number_summary.py
@@ -0,0 +1,19 @@
+def five_number_summary(list1):
+  #median
+  list1.sort()
+  if len(list1) % 2 == 0:
+    median = (list1[len(list1)//2] + list1[len(list1)//2-1])/2
+  else:
+    median = list1[len(list1)//2]
+  #max
+  max_val = max(list1)
+  #min
+  min_val = min(list1)
+
+  #lower quartile
+  q1 = round((25/100)*(max_val + min_val),2)
+  
+  #upper quartile
+  q3 = round((75/100)*(max_val + min_val),2)
+
+  return ({'Minimum':min_val,'Lower quartile':q1,'Median':median,'Upper quartile':q3,'Maximum':max_val})
