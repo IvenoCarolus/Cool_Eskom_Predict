bc696993b3964fcf0d243463da1b57aacec54a8e
Iveno Carolus <46677252+IvenoCarolus@users.noreply.github.com>
2020-02-03 13:28:58 +0200

    I removed the mode

--- function_1.py
+++ function_1.py
@@ -5,12 +5,6 @@ def metric_dic(list1):
   for i in list1:
     mean += i
   mean = round(mean / len(list1),2)
-
-  #mode
-  mode = 0
-  for i in list1:
-    if list1.count(i) > mode:
-      mode = i
   #median
   list1.sort()
   if len(list1) % 2 == 0:
@@ -26,4 +20,4 @@ def metric_dic(list1):
 
   #std
   std = round(var ** (1/2),2)
-  print({'Mean':mean,'Mode':mode,'Median':median,'Maximum':max_val,'Minimum':min_val,'Variance':var,'Standard Deviation':std})
+  print({'Mean':mean,'Median':median,'Maximum':max_val,'Minimum':min_val,'Variance':var,'Standard Deviation':std})
